7731b42643780e832cd4c70e0a84bc423dbb5c02
TheBigMort <91311589+TheBigMort@users.noreply.github.com>
2021-09-24 02:58:40 -0500

    Add files via upload

---
+++ data_parse.py
@@ -0,0 +1,117 @@
+'''
+title           : data_parse.py
+description     : Kong data from the OpenSea API is parsed with this program
+author          : TheBigMort
+date_created    : 20210627
+date_modified   : 20210805
+version         : 2.0
+python_version  : 3.9
+'''
+
+global shooting_stat, defense_stat, vision_stat, finish_stat
+
+def parse_kong_data(kong_dict):
+
+    stat = []
+    stat.clear()
+    kong_id = kong_dict['token_id']
+    owner_address = kong_dict['owner']['address']
+    traits = kong_dict['traits']
+    sell_orders = kong_dict['sell_orders']
+    rarity_url = 'https://rarity.tools/rumble-kong-league/view/{}'.format(kong_id)
+
+
+    def constructor(stat):
+        try:
+            for s in range(len(traits)):
+                if traits[s]["trait_type"] == stat:
+                    return traits[s]['value']
+        except:
+            return 'ERROR'
+    try:
+        last_sale_time = kong_dict['last_sale']['event_timestamp']
+    except:
+        last_sale_time = ' '
+    try:
+        last_sale_price = int(kong_dict['last_sale']['total_price'])
+        last_sale_price = last_sale_price/1000000000000000000
+        last_sale_price = '{} ETH'.format(last_sale_price)
+    except:
+        last_sale_price = ' '
+    try:
+        cprice = [a_dict['current_price'] for a_dict in sell_orders]
+        current = cprice[0]
+        current_price = str(int(current)/1000000000000000000)
+        current_price_eth = '{} ETH'.format(current_price)
+    except:
+        current_price_eth = ' '
+    try:
+        cdate = [[a_dict['created_date'] for a_dict in sell_orders]]
+        list_date = cdate[0][0]
+        list_date = list_date.replace('T', ' ')
+    except:
+        list_date = ' '
+    shooting_stat = constructor('Shooting')
+    finish_stat = constructor('Finish')
+    defense_stat = constructor('Defense')
+    vision_stat = constructor('Vision')
+    cumulative_stat = shooting_stat+finish_stat+defense_stat+vision_stat
+    result = {'rarity_url': rarity_url,
+              'kong_id': kong_id,
+              'owner_address': owner_address,
+              'num_sales': kong_dict['num_sales'],
+              'last_sale_date': last_sale_time[:10],
+              'last_sale_price': last_sale_price,
+              'list_date': list_date[:19],
+              'list_price': current_price_eth,
+              'background': constructor('Background'),
+              'fur': constructor('Fur'),
+              'clothes': constructor('Clothes'),
+              'mouth': constructor('Mouth'),
+              'head': constructor('Head'),
+              'head_accessory': constructor('Head Accessory'),
+              'eyes': constructor('Eyes'),
+              'jewellery': constructor('Jewellery'),
+              'shooting': constructor('Shooting'),
+              'finish': constructor('Finish'),
+              'defense': constructor('Defense'),
+              'vision': constructor('Vision'),
+              'cumulative': cumulative_stat}
+    return result
+
+def parse_sale_data(kong_dict):
+
+    sell_orders = kong_dict['sell_orders']
+    kong_id = kong_dict['token_id']
+
+
+    try:
+        last_sale_time = kong_dict['last_sale']['event_timestamp']
+        last_sale_price = int(kong_dict['last_sale']['total_price'])
+        last_sale_price = last_sale_price / 1000000000000000000
+        last_sale_price = '{} ETH'.format(last_sale_price)
+    except:
+        last_sale_time = ' '
+        last_sale_price = ' '
+    try:
+        cprice = [a_dict['current_price'] for a_dict in sell_orders]
+        current = cprice[0]
+        current_price = str(int(current)/1000000000000000000)
+        current_price_eth = '{} ETH'.format(current_price)
+    except:
+        current_price_eth = ' '
+    try:
+        cdate = [[a_dict['created_date'] for a_dict in sell_orders]]
+        list_date = cdate[0][0]
+        list_date = list_date.replace('T', ' ')
+    except:
+        list_date = ' '
+
+    result = {'kong_id': kong_id,
+              'num_sales': kong_dict['num_sales'],
+              'last_sale_date': last_sale_time[:10],
+              'last_sale_price': last_sale_price,
+              'list_date': list_date[:19],
+              'list_price': current_price_eth}
+
+    return result
---
+++ main.py
@@ -0,0 +1,61 @@
+'''
+title           : main.py
+description     : main file for kong_data_miner project
+author          : Adil Moujahid
+modified_by     : TheBigMort
+date_created    : 20210627
+date_modified   : 20210805
+version         : 2.0
+python_version  : 3.9
+'''
+
+from data_parse import parse_sale_data
+import requests
+import time
+
+from pymongo import MongoClient
+
+import matplotlib.pyplot as plt
+
+plt.style.use('ggplot')
+
+# Infinite loop to constantly run and update database
+while True:
+    # track time to check how long the program takes to run
+    start_time = time.time()
+    client = MongoClient({MongoDB_DB_Connection_String})
+    dbs = client.salesDB
+    # these three lines ensure the collection is cleared to prevent duplicate entries
+    kongs_sales = dbs.kongsSales
+    kongs_sales.drop()
+    kongs_sales = dbs.kongsSales
+
+    url = "https://api.opensea.io/api/v1/assets"
+
+    for i in range(0, 334):
+        querystring = {"token_ids": list(range((i * 30), (i * 30) + 30)),
+                       "asset_contract_address": "0xef0182dc0574cd5874494a120750fd222fdb909a",
+                       "order_direction": "desc",
+                       "offset": "0",
+                       "limit": "50"}
+        response = requests.request("GET", url, params=querystring)
+
+        print(i, end=" ")
+        if response.status_code != 200:
+            print('ERROR, RESPONSE CODE:')
+            print(response.status_code)
+            break
+
+        # Getting kongs data
+        kongs = response.json()['assets']
+        # Parsing kongs data
+        #parsed_kongs = [parse_kong_data(kong) for kong in kongs]
+        parsed_sales = [parse_sale_data(kong) for kong in kongs]
+        #storing parsed kongs data into MongoDB
+        #kongs_collection.insert_many(parsed_kongs)
+        kongs_sales.insert_many(parsed_sales)
+
+    end_time = time.time()
+    time_elapsed = end_time - start_time
+    print(time_elapsed)
+    time.sleep(300)
